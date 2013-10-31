6e8598ed01e85a4cb2a5ac5c9f9e9cf90b2ec18b
arnoux <arnoux@yahoo-inc.com>
2013-10-31 16:17:26 -0700

    initial commit

---
+++ yui_modules/request-cache.js
@@ -0,0 +1,148 @@
+/*jslint nomen:true, indent: 4, plusplus: true, forin: true, regexp:true */
+
+/*
+ * Copyright (c) 2013 Yahoo! Inc. All rights reserved.
+ */
+YUI.add('request-cache', function (Y, NAME) {
+    'use strict';
+
+    var originalDispatcher     = Y.mojito.Dispatcher,
+        OriginalActionContext  = Y.mojito.ActionContext,
+        RequestCacheDispatcher = function () {
+
+            this.dispatch = function (command, adapter) {
+
+                var cache,
+                    cachedResource,
+                    newCommand,
+                    freshInstance = command.instance;
+
+                // Build cache if it doesn't exist.
+                adapter.req.globals = adapter.req.globals || {};
+
+                if (!adapter.req.globals['request-cache']) {
+
+                    adapter.req.globals['request-cache'] = {
+                        addons: {},
+                        mojits: {
+                            byBase: {},
+                            byType: {}
+                        }
+                    };
+                }
+
+                // Retrieve the cache and try to get a corresponding cached resource.
+                cache = adapter.req.globals['request-cache'];
+                cachedResource = (freshInstance.base && cache.mojits.byBase[freshInstance.base]) ||
+                    (freshInstance.type && cache.mojits.byType[freshInstance.type]);
+
+                // If the is a cached resource, dispatch with that.
+                if (cachedResource) {
+
+                    // Merge the cached command and the fresh command
+                    newCommand = Y.merge(cachedResource.actionContext.command, command);
+
+                    // That was brutal - the cached command had properties that we wanted
+                    // but they got overwritten by smaller objects from the fresh, unexpanded command.
+                    // But we can' use Y.mix with the more delicate (recursive) merging because
+                    // the command may contain circular references. So now we need to cherry-pick.
+                    //
+                    // We need to mix-in the expanded instance from the cache without overwriting
+                    // the properties from the fresh command.instance.
+                    //  Here again we can't use merge because of circular references.
+                    Y.mix(newCommand.instance, cachedResource.actionContext.command.instance);
+
+                    // So we didn't overwite the instance.config, but the cached instance bears
+                    //  some config that we want to retain, but we don't overwite to give
+                    //  priority to the fresh config.
+                    //  We're assuming there isn't any circular references here, so we merge.
+                    Y.mix(newCommand.instance.config, cachedResource.actionContext.command.instance.config, false, null, 0, true);
+
+                    // The cached AC gets the new command.
+                    cachedResource.actionContext.command = newCommand;
+
+                    // Instantiate again the addons that need to be refreshed
+                    Y.Array.each(['config', 'params'], function (addonName) {
+                        var addonInstance,
+                            AddonConstuct = Y.mojito.addons.ac[addonName];
+
+                        if (AddonConstuct) {
+                            addonInstance = new AddonConstuct(newCommand, adapter, cachedResource.actionContext);
+
+                            if (addonInstance.namespace && cachedResource.actionContext[addonInstance.namespace]) {
+
+                                cachedResource.actionContext[addonInstance.namespace] = addonInstance;
+
+                                if (Y.Lang.isFunction(addonInstance.setStore)) {
+                                    addonInstance.setStore(cachedResource.store);
+                                }
+                            }
+                        }
+                    });
+
+                    // The adapter and its callbacks need to be refreshed
+                    cachedResource.actionContext._adapter = adapter;
+
+                    // @see: ActionContext constructor
+                    // TODO: handle __call
+                    // TODO: handle staticAppConfig.actionTimeout
+                    if (Y.Lang.isFunction(cachedResource.controller[newCommand.action])) {
+                        cachedResource.controller[newCommand.action](cachedResource.actionContext);
+                    }
+                } else {
+
+                    // Expands the command.instance and creates a new AC
+                    //  which in turn instanciates all the addons and calls the controller
+                    originalDispatcher.dispatch.apply(this, arguments);
+                }
+            };
+        },
+
+        ExpandedResource = function (options) {
+            this.actionContext = options.actionContext;
+            this.controller    = options.controller;
+            this.store         = options.store;
+        },
+        /**
+         * A superclass for mojito's ActionContext
+         * @param {Object} options.controller
+         * @param {Object} options.command
+         * @param {Object} options.store
+         * @param {Object} options.adapter
+         * @param {Object} options.dispatcher
+         */
+        RequestCacheActionContext = function (options) {
+            var newExpandedResource = new ExpandedResource({
+                    actionContext: this,
+                    controller: options.controller
+                }),
+                instance = options.command.instance,
+                cache = options.adapter.req.globals['request-cache'];
+
+            // Save the references in either byBase or byType
+            if (instance.base) {
+                cache.mojits.byBase[instance.base] = newExpandedResource;
+            } else if (instance.type) {
+                cache.mojits.byType[instance.type] = newExpandedResource;
+            }
+
+            // Execute the original constructor: addons + controller call
+            OriginalActionContext.apply(this, arguments);
+        };
+
+    RequestCacheDispatcher.prototype    = originalDispatcher;
+    RequestCacheActionContext.prototype = OriginalActionContext.prototype;
+
+    /**
+     * A "superinstance" of mojito's Dispatcher
+     * @type {Object}
+     */
+    Y.mojito.Dispatcher    = new RequestCacheDispatcher();
+    Y.mojito.ActionContext = RequestCacheActionContext;
+
+}, '0.1.0', {
+    requires: [
+        'mojito-dispatcher',
+        'mojito-action-context'
+    ]
+});
